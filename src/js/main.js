a8a4f3d13c3fc89bccbcbbcfcfda05c4ba071411
Sampsa Saari <sampsa.saari@gmail.com>
2018-04-04 20:26:34 +0300

    Changed Font Awesome to use NPM. Fixed social media links.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
 import './../components/general/scroll';
 import './../components/general/lazyload';
+import fontawesome from '@fortawesome/fontawesome';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+
+fontawesome.library.add(faTwitter, faLinkedinIn, faGithub);
